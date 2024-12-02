8a57cc103e0f57c66cf1ba668066e34729728895
Daniel Umpierrez <10012416+havocesp@users.noreply.github.com>
2024-12-02 19:59:01 +0000

    Update setup.py
    Added classifiers for new python versions.

--- setup.py
+++ setup.py
@@ -22,8 +22,11 @@ setup(
     license_files=["LICENSE"],
     classifiers=[
         "Intended Audience :: Financial and Insurance Industry",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
         "License :: OSI Approved :: MIT License",
     ],
     project_urls={
